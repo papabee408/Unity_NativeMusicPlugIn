a7b49b2b365e6f108302fc8c164c998d16c74956
Noah <64256495+papabee408@users.noreply.github.com>
2020-10-10 21:18:12 +0900

    Add files via upload

---
+++ test_script.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class test_script : MonoBehaviour
+{
+    public Text text;
+    AndroidJavaClass plugin;
+    AndroidJavaObject pluginObject;
+
+    string testURL = "";
+
+    private void Start()
+    {
+        plugin = new AndroidJavaClass("com.example.musicplayer.Musicplayer");
+        pluginObject = new AndroidJavaObject("com.example.musicplayer.Musicplayer");
+        pluginObject.Call("StreamMedia", testURL);
+    }
+
+    private void OnDestroy()
+    {
+        text.text = pluginObject.Call<string>("onDestroy");
+    }
+
+    public void PlayMusic()
+    {
+        text.text = pluginObject.Call<string>("playAudio", testURL);
+    }
+
+    public void PauseMusic()
+    {
+        text.text = pluginObject.Call<string>("pauseAudio");
+    }
+
+    public void RestartMusic()
+    {
+        text.text = pluginObject.Call<string>("restart");
+    }
+
+    public void GetDuration()
+    {
+        text.text = pluginObject.Call<int>("getDuration").ToString();
+    }
+
+    public void GetCurrentPosition()
+    {
+        text.text = pluginObject.Call<int>("getCurrentPosition").ToString();
+    }
+}
